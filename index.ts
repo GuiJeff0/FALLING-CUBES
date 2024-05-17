e9d0580300b52c1f775c0593a8c619572e193e09
Guilherme Santos <guilhermejeffofc@gmail.com>
2024-05-17 15:16:05 -0300

    project-dayone-update-indexTs

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import * as B from "babylonjs";
 import "regenerator-runtime/runtime";
 import { scene, engine } from "./src/scene";
+import { makeGround } from "./src/ground";
 import Ammo from "ammojs-typed";
+import { makeCube } from "./src/cube";
 
 async function main(): Promise<void> {
     const ammo = await Ammo();
     const physics: B.AmmoJSPlugin = new B.AmmoJSPlugin(true, ammo);
     scene.enablePhysics(new B.Vector3(0,-9.81,0), physics);
+
+    makeCube();
+    makeGround();
+
     engine.runRenderLoop(() => scene.render());
 }
 
